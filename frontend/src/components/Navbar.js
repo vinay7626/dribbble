3b083cd1af3fa1bbb86d017f943f27a8b9b92892
vinay7626 <97014123+vinay7626@users.noreply.github.com>
2024-04-10 21:12:05 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -16,11 +16,11 @@ function Navbar(){
                     <button onClick={() => {
                         console.log(showNav);
                         setShowNav(!showNav);
-                        }}><FontAwesomeIcon icon={faBars} className='py-7 px-3 sm:text-3xl text-lg' /></button>
+                        }}><FontAwesomeIcon icon={faBars} className='py-7 px-2 sm:text-3xl text-lg' /></button>
                 </div>
                 <div className='flex-none place-content-center'>
                     
-                    <img src={dribbbleLogoDark} alt="logo" className='p-2 sm:w-32 w-24 flex-none'/>
+                    <img src={dribbbleLogoDark} alt="logo" className='p-2 sm:w-32 w-20 flex-none'/>
                 </div>
                 <div className={showNav?"flex-1 place-content-center lg:visible":"hidden lg:visible"}>
                     <ul className='lg:flex lg:flex-row lg:gap-10 lg:justify-evenly lg:static lg:border-none flex-col absolute place-self-center gap-y-20 left-0 top-20 bg-white divide-y max-lg:border-slate-300 max-lg:border-2 px-8'>
@@ -47,13 +47,13 @@ function Navbar(){
                 </div>
                 <div className='flex flex-row-reverse place-content-center '>
                     <div className="p-3">
-                        <button className='sm:py-3 sm:px-6 py-1 px-3 mt-3 sm:mt-0 bg-pink-400 rounded-lg text-white font-bold place-self-center'>Upload</button>
+                        <button className='sm:py-3 sm:px-6 py-1 px-1 mt-3 sm:mt-0 bg-pink-400 rounded-lg text-white font-bold place-self-center'>Upload</button>
                     </div>
                     <div className="place-content-center">
-                        <img className="sm:size-16 size-14 p-2 rounded-full overflow-hidden min-w-14" src={userProfile.image} alt="profile" />
+                        <img className="sm:size-16 size-14 p-1 rounded-full overflow-hidden min-w-14" src={userProfile.image} alt="profile" />
                     </div>
                     <div className="place-content-center p-2">
-                        <FontAwesomeIcon icon={faBriefcase} className='sm:text-3xl text-2xl p-2 text-slate-300' />
+                        <FontAwesomeIcon icon={faBriefcase} className='sm:text-3xl text-xl p-1 text-slate-300' />
                     </div>
                 </div>
             </nav>
