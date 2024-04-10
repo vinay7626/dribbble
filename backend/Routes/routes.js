4d6e2b723edff31b67f146b85864032d1a1a8c1b
vinay7626 <97014123+vinay7626@users.noreply.github.com>
2024-04-10 22:23:34 +0530

    Update routes.js

--- backend/Routes/routes.js
+++ backend/Routes/routes.js
@@ -95,7 +95,7 @@ router.put("/update-user",async (req,res) => {
         });
         if(error){
             console.log(error);
-            res.status(403).send(error.message);
+            res.status(403).json({data:error.message});
         }else{
         console.log("Email sent");
         res.status(200).send(updatedUser);
