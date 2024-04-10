09f2067171d118684c245f8cfeb46a0317b6d16b
vinay7626 <97014123+vinay7626@users.noreply.github.com>
2024-04-10 17:33:17 +0530

    removed everything deployment issue

--- backend/DB/connection.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config()
-
-console.log(process.env.DB);
-mongoose.connect(process.env.DB, {useNewUrlParser: true,})
-  .then(() => console.log("connection start")).catch((err) => console.log(err));
--- frontend/src/contexts/UserProfileContext.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const UserProfileContext = React.createContext();
-
-export default UserProfileContext;
