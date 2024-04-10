67bc4a903b8335dd41608e8e9d5858f570daf611
vinay7626 <97014123+vinay7626@users.noreply.github.com>
2024-04-10 21:49:24 +0530

    Update Options.js

--- frontend/src/views/Options.js
+++ frontend/src/views/Options.js
@@ -37,7 +37,7 @@ function Options() {
             axios.put(`${process.env.REACT_APP_SERVER_API}/update-user`,{userProfile})
             .then((res) => console.log(res))
             .then(() => navigate('/done'))
-            .catch((err) => alert(err.message);
+            .catch((err) => alert(err.message));
         }else{
             return
         }
