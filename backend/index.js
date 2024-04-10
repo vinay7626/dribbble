5f784fd46ba65be4f3141a33813a8302d5630171
vinay7626 <97014123+vinay7626@users.noreply.github.com>
2024-04-10 17:34:02 +0530

    added backend folder

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+require('./DB/connection');
+const router = require('./Routes/routes');
+const cors = require("cors");
+app.use(express.urlencoded({extended:true}))
+const bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({ extended: true }));
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+};
+
+app.use(express.json());
+
+app.use(cors(corsOptions));
+
+app.use(router);
+
+
+app.listen(PORT,() => {
+    console.log(`running on ${PORT}`)
+})
